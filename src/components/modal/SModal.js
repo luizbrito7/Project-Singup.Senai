87f43df8d6dff5ecd7f4554aaad5c0f093543090
luizbrito6 <luizg1717@gmail.com>
2023-12-05 19:43:10 -0300

    fix: modal in hero 3/3

--- src/components/modal/SModal.js
+++ src/components/modal/SModal.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { flex } from '../style'
+import { flex, primaryColor } from '../style'
 
 const SModal = styled.div`
     position: fixed;
@@ -7,16 +7,75 @@ const SModal = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
-    background: rgba(0, 0, 0, 0.5);
-    ${flex('column', 'center', 'center')};
     transform: translateY(-100%);
-    transition: transform 0.3s ease-out;
-
+    background: rgba(0, 0, 0, 0.7);
+    ${flex('column', 'center', 'center')};
+    transition: transform .2s ease-in-out;
+    
     div{
-        background-color: green;
         ${flex('column', 'center', 'center')};
+        gap: 12rem;
+
+        button{
+            background-color: #fff;
+            padding: 8rem 12rem;
+            border-radius: 6rem;
+            border:  none;
+            cursor: pointer;
+            font-weight: 700;
+            transition: all 300ms ease-in-out;
+            color: gray;
+
+            &:hover{
+                background-color: ${primaryColor};
+                color: #fff;
+            }
+        }
+
+        iframe{
+            width: 935rem;
+            height: 526rem;
+        }
+
+      
     }
 
+    @media (max-width: 1000px) {
+
+        div{
+            iframe{
+            width: 735rem;
+            height: 426rem;
+            }  
+        }      
+    }
+
+    @media (max-width: 780px) {
+        div{
+            iframe{
+            width: 535rem;
+            height: 326rem;
+            }  
+        }
+    }
+
+    @media (max-width: 600px) {
+        div{
+            iframe{
+            width: 435rem;
+            height: 226rem;
+            }  
+        }
+    }
+
+    @media (max-width: 450px) {
+        div{
+            iframe{
+            width: auto;
+            height: auto;
+            }  
+        }
+    }
 
 `
 
